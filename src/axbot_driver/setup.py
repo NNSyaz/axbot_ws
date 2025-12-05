96f2a4326860e0fc258f1e36fecfd6f4bba778ac
NNSyaz <syazwaninurul02@gmail.com>
2025-12-05 17:30:47 +0800

    Update 0.01

--- src/axbot_driver/setup.py
+++ src/axbot_driver/setup.py
@@ -18,6 +18,7 @@ setup(
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
